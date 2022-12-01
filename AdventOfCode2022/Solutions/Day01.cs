dc29a90f2947a5e55588261705ecb6b9c5b8cdd3
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2022-12-01 12:56:32 +0000

    Add project files.

---
+++ AdventOfCode2022/Solutions/Day01.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2022.Solutions
+{
+    internal class Day01
+    {
+        public int Part1(string input)
+        {
+            var elves = ParseInput(input);
+            return elves.Max();
+        }
+
+        public int Part2(string input)
+        {
+            var elves = ParseInput(input);
+            return elves.OrderDescending().Take(3).Sum();
+        }
+
+        public IEnumerable<int> ParseInput(string input)
+        {
+            return input.Split($"{Environment.NewLine}{Environment.NewLine}")
+                .Select(x => x.Split(Environment.NewLine).Select(int.Parse))
+                .Select(x => x.Sum());
+        }
+    }
+}
---
+++ AdventOfCode2022/Tests/Day01Tests.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode2022.Solutions;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestClass]
+    public class Day01Tests
+    {
+        private string Sample =
+            "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000";
+    
+        private string Input = File.ReadAllText("Input/Day01.txt");
+
+        private Day01 _day01 = new();
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day01 = new Day01();
+        }
+
+        [TestMethod]
+        public void Part1Sample()
+        {
+            Assert.AreEqual(24000, _day01.Part1(Sample));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            Assert.AreEqual(72602, _day01.Part1(Input));
+        }
+
+        [TestMethod]
+        public void Part2Sample()
+        {
+            Assert.AreEqual(45000, _day01.Part2(Sample));
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            Assert.AreEqual(207410, _day01.Part2(Input));
+        }
+    }
+}
