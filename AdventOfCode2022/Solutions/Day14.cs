f78aae1a7a6dbb5f18f57d391b2c8b9793ce1d66
mattsharpe <mattsharpe@users.noreply.github.com>
2022-12-19 11:59:47 +0000

    d14

---
+++ AdventOfCode2022/Solutions/Day14.cs
@@ -0,0 +1,16 @@
+
+namespace AdventOfCode2022.Solutions
+{
+    internal class Day14
+    {
+        public int Part1(string[] input)
+        {
+           return 0;
+        }
+
+        public int Part2(string[] input)
+        {
+           return 0;
+        }
+    }
+}
